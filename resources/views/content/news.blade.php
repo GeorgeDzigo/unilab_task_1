018d722ff6480089391aacc4d2fb501dc66a2fca
GeorgeDzigo <mr.georgegamertv@gmail.com>
2021-03-17 20:43:44 +0200

    DONE

---
+++ resources/views/content/news.blade.php
@@ -0,0 +1,5 @@
+@extends('news')
+
+@section('content')
+<h1>News</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/news', function () {
+    return view("content.news");
+})->name("news");
+
+Route::get("/news/{id}", function($id) {
+    return view("content.newsid", ["id" => $id]);
+});
+
+Route::get("/other", function() {
+    return redirect(route("news"));
+});
+Route::get('/404', function() {
+    return abort(404, "Page not found");
 });
